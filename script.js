919265ae9f812b036a5633eb3aa370a74f7a71a9
safetyrob <90059468+safetyrob@users.noreply.github.com>
2021-09-09 07:44:17 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -14,22 +14,13 @@ function displayPhotos() {
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
     const item = document.createElement("a");
-    setAttributes(item, {
-      href: photo.links.html,
-      target: "_blank",
-    });
-    // item.setAttribute("href", photo.links.html);
-    // item.setAttribute("target", "_blank");
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
     //Create <img> for photo
     const img = document.createElement("img");
-    setAttributes(img, {
-      src: photo.urls.regular,
-      alt: photo.alt_description,
-      title: photo.alt_description,
-    });
-    // img.setAttribute("src", photo.urls.regular);
-    // img.setAttribute("alt", photo.alt_description);
-    // img.setAttribute("title", photo.alt_description);
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
